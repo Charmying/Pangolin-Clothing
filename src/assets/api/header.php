4f0cc326b312ecd1686e218f186523ebcf8d8687
Charmy_Tseng <charmytseng0118@gmail.com>
2023-06-24 03:54:10 +0800

    修改 header.php -v2

--- src/assets/api/header.php
+++ src/assets/api/header.php
@@ -1,4 +1,12 @@
 <?php
+
+$Origin = isset($_SERVER['HTTP_ORIGIN'])?$_SERVER['HTTP_ORIGIN']:"*";
+header("Access-Control-Allow-Origin: {$Origin}");
+header("Access-Control-Allow-Methods:POST,GET,OPTIONS");
+header("Access-Control-Allow-Credentials:true");
+
+
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
