c0f255910d305dc45ceff3237c59426d456aa805
Charmy_Tseng <charmytseng0118@gmail.com>
2023-06-24 15:51:47 +0800

    改 fetch URL

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,4 +1,4 @@
 // 測 render 用
-export const BASE_URL = "https://pangolin-clothing.onrender.com/src/assets/api";
+// export const BASE_URL = "https://pangolin-clothing.onrender.com/api";
 // 測 Localhost 用
-// export const BASE_URL = "http://localhost/pangolin-clothing/src/assets/api";
+export const BASE_URL = "http://localhost/pangolin-clothing/public/api";
